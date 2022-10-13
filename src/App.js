c0b2a018a5ce82f36dbb0bbc96604de3ef9cd436
Manuel <manekete@gmail.com>
2022-10-12 19:54:45 -0600

    (intro hooks) using sync

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 const App = () => {
+  const [role, setRole] = useState('dev');
+
   const showEmployees = true;
+
   return (
     <div className="App">
       {showEmployees ? (
         <div>
-          <Employee />
-          <Employee />
-          <Employee />
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name={'Julia'} role="Intern" />
+          <Employee name={'Monica'} role={role} />
+          <Employee name={'Erika'} />
         </div>
       ) : (
         <p>You cannot see the employees </p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Employee = () => {
-  return <h3>Here is Emplloye </h3>;
+const Employee = (props) => {
+  return (
+    <>
+      <h3>Employee {props.name}</h3>
+      <p>{props.role ? props.role : 'No role'}</p>
+    </>
+  );
 };
 
 export default Employee;
